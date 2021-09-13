74326cf1890bc933c1535b6caa8ad0017f2c2d60
Brandon M <bdmolyne@gmail.com>
2021-09-13 16:05:41 -0400

    initial commit with test file

---
+++ main.py
@@ -0,0 +1,9 @@
+
+class Board:
+    width = 3
+    height = 3
+
+
+class Players:
+    n = 2 # 2 players
+    which_player = 1 # 1 for player 1, 2 for player 2.
---
+++ test_main.py
@@ -0,0 +1,19 @@
+import unittest
+import main
+
+class TestMain(unittest.TestCase):
+
+    def setUp(self):
+        self.board = main.Board()
+        self.players = main.Players()
+
+    def test_board_is_3_by_3(self):
+        self.assertEqual(self.board.width, 3)
+        self.assertEqual(self.board.height, 3)
+
+    def test_2_players(self):
+        self.assertEqual(self.players.n, 2)
+
+
+if __name__ == "__main__":
+    unittest.main()
