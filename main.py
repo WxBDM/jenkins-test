dae72ab62f56bd541712154ab168b87fbe042840
Brandon M <bdmolyne@gmail.com>
2021-09-13 17:29:07 -0400

    code to test if user won implemented

--- main.py
+++ main.py
@@ -10,22 +10,75 @@ class Board:
 
     def __init__(self):
         """Instantiates a board object."""
-        # 0 is empty, 1 is player 1, 2 is player 2.
-        self._board = [0] * (self.width * self.height)
+        # None is empty, 1 is player 1, 2 is player 2.
+        self._board = [None] * (self.width * self.height)
 
     def _check_if_board_is_empty(self):
         if 0 in self._board:
             return False
         return True
 
+    def _check_indexes(self, l):
+        # If the set is 2 elements, no winner.
+        # If the set is 1 element but with None, then no winner.
+        check = {self._board[l[0]], self._board[l[1]], self._board[l[2]]}
+        if len(check) == 1 and list(check)[0] is not None:
+            return True
+
+        return False
+
     def _check_for_diagonal_win(self):
-        pass
+        """Checks diagonals for winning player"""
+        # Check UL to LR
+        is_winner = self._check_indexes([0, 4, 8])
+        if is_winner:
+            return True
+
+        # Check UR to LL
+        is_winner = self._check_indexes([2, 4, 6])
+        if is_winner:
+            return True
+
+        # No winner.
+        return False
 
     def _check_for_horizontal_win(self):
-        pass
+        # Check row 1
+        is_winner = self._check_indexes([0, 1, 2])
+        if is_winner:
+            return True
+
+        # Check row 2
+        is_winner = self._check_indexes([3, 4, 5])
+        if is_winner:
+            return True
+
+        # Check row 3
+        is_winner = self._check_indexes([6, 7, 8])
+        if is_winner:
+            return True
+
+        # No winner.
+        return False
 
     def _check_for_vertical_win(self):
-        pass
+        # Check column 1
+        is_winner = self._check_indexes([0, 3, 6])
+        if is_winner:
+            return True
+
+        # Check row 2
+        is_winner = self._check_indexes([1, 4, 7])
+        if is_winner:
+            return True
+
+        # Check row 3
+        is_winner = self._check_indexes([2, 5, 8])
+        if is_winner:
+            return True
+
+        # No winner.
+        return False
 
 class Players:
     n = 2 # 2 players
