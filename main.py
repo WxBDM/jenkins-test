1335185a37d558260401177d12a7caa6fd9333cd
Brandon M <bdmolyne@gmail.com>
2021-09-13 16:37:59 -0400

    added more test cases

--- main.py
+++ main.py
@@ -3,7 +3,24 @@ class Board:
     width = 3
     height = 3
 
+    def __init__(self):
+        """Instantiates a board object."""
+        # 0 is empty, 1 is player 1, 2 is player 2.
+        self._board = [0] * (width * height)
+
+    def _check_if_board_is_empty(self):
+        if 0 in self._board:
+            return False
+        return True
 
 class Players:
     n = 2 # 2 players
     which_player = 1 # 1 for player 1, 2 for player 2.
+
+    def next_player(self):
+        """Switches players"""
+
+        if self.which_player == 1:
+            self.which_player = 2
+        else:
+            self.which_player = 1
--- test_main.py
+++ test_main.py
@@ -8,14 +8,51 @@ class TestMain(unittest.TestCase):
         self.players = main.Players()
 
     def test_board_is_3_by_3(self):
+        """Ensures the board is 3x3"""
+
         self.assertEqual(self.board.width, 3)
         self.assertEqual(self.board.height, 3)
 
     def test_2_players(self):
+        """Tests to make sure there's only 2 players"""
+
         self.assertEqual(self.players.n, 2)
 
-    def test_this_will_fail(self):
-        self.assertEqual(2, 3)
+    def test_player_one_goes_first(self):
+        """Ensures player 1 goes first"""
+
+        self.assertEqual(self.players.current_player, 1)
+
+    def test_player_two_goes_second(self):
+        """Tests to make sure player 2 goes second"""
+        self.players.next_player()
+        self.assertEqual(self.players.current_player, 2)
+
+    def test_player_one_turn_after_player_two(self):
+        """Tests to make sure player 1 goes after player 2"""
+        self.players.next_player()
+        self.players.next_player()
+        self.assertEqual(self.players.current_player, 1)
+
+    def board_is_dstructure_list(self):
+        """Ensures the board is a list"""
+        self.assertTrue(isinstance(self.board._board, list))
+
+    def board_is_full(self):
+        """Forces the board to be full, then checks to make sure it is"""
+        # should return true if there is not a 0 in the board.
+        board._board = [1] * 9
+        self.assertTrue(board._check_if_board_is_empty())
+
+    def board_has_at_least_one_slot_empty(self):
+        """Forces the board to have at least one slot empty"""
+        board._board = [1] * 9
+        board._board[0] = 0
+        self.assertFalse(board._check_if_board_is_empty())
+
+    def tearDown(self):
+        self.board = main.Board()
+        self.players = main.Players()
 
 
 if __name__ == "__main__":
